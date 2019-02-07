55555767ac142299151fb259657d98d997f304d9
Naveen Kamrudeen <Naveen.Kamrudeen1@aexp.com>
2019-02-06 20:54:00 -0700

    Insert

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_NAME='airflow'
+PROJECT_NAME='airflow-celery'
 
 # Create the base-container project that contains the images
 oadm new-project $PROJECT_NAME --display-name=$PROJECT_NAME --description='Apache Airflow on OpenShift'
@@ -8,6 +8,11 @@ oadm new-project $PROJECT_NAME --display-name=$PROJECT_NAME --description='Apach
 # dependencies
 oc create -f deployments/airflow-secrets.yaml -n $PROJECT_NAME
 oc create -f deployments/airflow-pv.yaml -n $PROJECT_NAME
+oc create sa airflow-celery -n $PROJECT_NAME
+oc create role podcrud-celery --verb=get,create,list,watch,patch,delete,update --resource=pod -n $PROJECT_NAME
+oc adm policy add-role-to-user podcrud-celery system:serviceaccount:airflow-celery:$PROJECT_NAME --role-namespace=$PROJECT_NAME -n $PROJECT_NAME
+oc create -f deployments/airflow-role.yaml -n $PROJECT_NAME
+oc adm policy add-role-to-user podlogsreader system:serviceaccount:airflow-celery:$PROJECT_NAME --role-namespace=$PROJECT_NAME -n $PROJECT_NAME
 
 # imagestream builds
 oc create -f deployments/airflow-build.yaml -n $PROJECT_NAME
